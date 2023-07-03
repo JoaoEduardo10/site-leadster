2441b1ca92bac46bcb62bd6bcb0ebcc6b8950740
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-03 19:08:58 -0300

    feat(HeaderCaption): adding stories

---
+++ src/components/HeaderCaption/HeaderCaption.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { HeaderCaption } from '.';
+
+export default {
+  title: 'HeaderCaption',
+  component: HeaderCaption,
+} as Meta;
+
+export const StoryLogo = () => {
+  return <HeaderCaption />;
+};
