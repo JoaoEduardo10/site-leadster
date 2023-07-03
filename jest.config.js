e4132b02dd0aa630e09391f7a0ebecae93bef5af
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-03 13:36:27 -0300

    feat(jest): adding initial configuration

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*mock*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/**/*stories*.{js,jsx,ts,tsx}',
+    '!<rootDir>/types/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/types/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/styles/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/utils/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/templates/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/app/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/config/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/src/pages/**/*.{js,jsx,ts,tsx}',
+    '!<rootDir>/**/stories.{js,jsx,ts,tsx}',
+    '!<rootDir>/node_modules/',
+  ],
+  testMatch: ['<rootDir>/src/components/**/*.test.tsx'],
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    '<rootDir>/.out/',
+    '/public/',
+  ],
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
