edfd71093b4a1037642191612a28a5b82b40182e
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-03 19:22:48 -0300

    feat(Header): adding stories

---
+++ src/components/header/Header.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { Header } from '.';
+
+export default {
+  title: 'Header',
+  component: Header,
+} as Meta;
+
+export const StoryHeader = () => {
+  return <Header />;
+};
