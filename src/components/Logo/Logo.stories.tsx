f2f1ad06e425fb0c817632725c20207fa7dfe9a5
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-03 16:51:28 -0300

    feat(Logo): adding Stories

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,15 @@
+import { Meta } from '@storybook/react';
+import { Logo, LogoProps } from '.';
+import logo from '../../../public/images/logo.png';
+
+export default {
+  title: 'Logo',
+  component: Logo,
+  args: {
+    img: logo,
+  } as LogoProps,
+} as Meta;
+
+export const StoryLogo = (agrs: LogoProps) => {
+  return <Logo {...agrs} />;
+};
