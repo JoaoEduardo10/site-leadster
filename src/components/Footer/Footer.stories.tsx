0e0efad0662f9a44b66c23b3d5517cb395788486
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-04 13:03:27 -0300

    feat(Footer): adding stories

---
+++ src/components/Footer/Footer.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { Footer } from '.';
+
+export default {
+  title: 'Footer',
+  component: Footer,
+} as Meta;
+
+export const StoryFooter = () => {
+  return <Footer />;
+};
