d02697516909993673f6ededb5d9c33fdc6c7b6c
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-04 12:48:06 -0300

    feat(Copyrigth): adding stories

---
+++ src/components/Copyright/Copyright.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { Copyrigth } from '.';
+
+export default {
+  title: 'Copyrigth ',
+  component: Copyrigth,
+} as Meta;
+
+export const StoryCopyrigth = () => {
+  return <Copyrigth />;
+};
