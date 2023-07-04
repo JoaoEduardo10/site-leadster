3585d0ac2a92a5503cd08fa25ff4587abe36aabb
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-04 16:39:12 -0300

    feat(NavLink): adding test

---
+++ src/components/NavLinks/NavLink.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { NavLink } from '.';
+
+export default {
+  title: 'NavLink',
+  component: NavLink,
+} as Meta;
+
+export const StoryNavLink = () => {
+  return <NavLink />;
+};
