77cf6d25229dfe7355fabaf47cf813f873675c09
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-04 16:38:14 -0300

    feat(menu): adding mock

---
+++ src/components/Menu/mock.ts
@@ -0,0 +1,24 @@
+import { MenuProps } from '.';
+
+export const mockMenu: MenuProps['links'] = [
+  {
+    link: '#agencias',
+    title: 'Agências',
+  },
+  {
+    link: '#chatbot',
+    title: 'Chatbot',
+  },
+  {
+    link: '#marketing',
+    title: 'Marketing Digital',
+  },
+  {
+    link: '/',
+    title: 'Geração de Leads',
+  },
+  {
+    link: '#midia-paga',
+    title: 'Mídia Paga',
+  },
+];
