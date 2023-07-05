5019968489ecd4cd2a75ac463d14b7fdd33bcb1b
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-05 18:41:22 -0300

    feat(LeadsGenerationBanner): adding stories

---
+++ src/components/LeadsGenerationBanner/LeadsGenerationBanner .stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { LeadsGenerationBanner } from '.';
+
+export default {
+  title: 'LeadsGenerationBanner',
+  component: LeadsGenerationBanner,
+} as Meta;
+
+export const StoryLeadsGenerationBanner = () => {
+  return <LeadsGenerationBanner />;
+};
