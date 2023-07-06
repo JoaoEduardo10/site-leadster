58c175bd49460247375a7224e6a06befa0917ac8
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-06 11:42:05 -0300

    refactory(VideoCards): ajust test

--- src/components/VideoCards/VideoCards.test.tsx
+++ src/components/VideoCards/VideoCards.test.tsx
@@ -1,15 +1,25 @@
 import { renderTheme } from '@/utils/render-theme';
 import { VideoCards } from '.';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+jest.useFakeTimers();
 
 describe('<VideoCards />', () => {
   it('should render the component', () => {
-    renderTheme(<VideoCards img={'http://image.png'} title="test" />);
+    renderTheme(
+      <VideoCards
+        description="test"
+        video="https://test"
+        img={'http://image.png'}
+        title="test"
+      />,
+    );
 
-    const videoCardsConteiner = screen.getByRole('article');
+    const videoCardsConteiner = screen.getByLabelText('card');
     const image = screen.getByRole('img');
-    const header = screen.getByRole('heading');
+    const header = screen.getByLabelText('title card');
 
     expect(videoCardsConteiner).toBeInTheDocument();
     expect(image).toBeInTheDocument();
@@ -17,9 +27,16 @@ describe('<VideoCards />', () => {
   });
 
   it('should activate the animation when hovering over the component', () => {
-    renderTheme(<VideoCards img={'http://image.png'} title="test" />);
+    renderTheme(
+      <VideoCards
+        description="test"
+        video="https://test"
+        img={'http://image.png'}
+        title="test"
+      />,
+    );
 
-    const videoCardsConteiner = screen.getByRole('article');
+    const videoCardsConteiner = screen.getByLabelText('card');
     const animation_video = screen.getByLabelText('animation video');
 
     expect(videoCardsConteiner).toBeInTheDocument();
@@ -29,5 +46,28 @@ describe('<VideoCards />', () => {
     expect(animation_video).toHaveStyle({
       display: 'block',
     });
+  });
+
+  it('should show the ExpandableCard component on click', () => {
+    renderTheme(
+      <VideoCards
+        description="test"
+        video="https://test"
+        img={'http://image.png'}
+        title="test"
+      />,
+    );
+
+    const videoCardsConteiner = screen.getByLabelText('card');
+
+    act(() => {
+      jest.advanceTimersByTime(500);
+    });
+
+    fireEvent.click(videoCardsConteiner);
+
+    const expandableCard = screen.getByLabelText('conteiner fixed');
+
+    expect(expandableCard).toBeInTheDocument();
   });
 });
