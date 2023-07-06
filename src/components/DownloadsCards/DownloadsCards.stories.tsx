05aaebe928b9a1296c977d51bb3b99f43ed03dbe
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-06 11:36:48 -0300

    feat(DownloadsCards): adding stories

---
+++ src/components/DownloadsCards/DownloadsCards.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta } from '@storybook/react';
+import { DownloadsCards } from '.';
+
+export default {
+  title: 'DownloadsCards',
+  component: DownloadsCards,
+} as Meta;
+
+export const StoryDownloadsCards = () => {
+  return <DownloadsCards />;
+};
