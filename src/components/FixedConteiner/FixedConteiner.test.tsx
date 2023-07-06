f1c8c7130d632603335778e4dff7fa31d4121bcb
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-06 11:39:41 -0300

    feat(FixedConteiner):  adding test

--- src/components/FixedConteiner/FixedConteiner.test.tsx
+++ src/components/FixedConteiner/FixedConteiner.test.tsx
@@ -3,11 +3,51 @@ import { FixedConteiner } from '.';
 import { screen } from '@testing-library/react';
 
 describe('<FixedConteiner />', () => {
-  it('Should render a FixedConteiner component', () => {
-    renderTheme(<FixedConteiner>Children</FixedConteiner>);
+  it('Should render a FixedConteiner component with animation', () => {
+    renderTheme(
+      <FixedConteiner rendered={true} show={true}>
+        Children
+      </FixedConteiner>,
+    );
 
     const conteiner = screen.getByText('Children');
 
     expect(conteiner).toBeInTheDocument();
+    expect(conteiner).toHaveStyleRule(
+      'animation',
+      'openShow 300ms ease-in-out',
+    );
+  });
+
+  it('should render a FixedConteiner component without animation', () => {
+    renderTheme(
+      <FixedConteiner rendered={true} show={false}>
+        Children
+      </FixedConteiner>,
+    );
+
+    const conteiner = screen.getByText('Children');
+
+    expect(conteiner).toBeInTheDocument();
+    expect(conteiner).toHaveStyleRule(
+      'animation',
+      'closeShow 0.5s ease-in-out',
+    );
+  });
+
+  it('should render a FixedConteiner component without animation and without rendering', () => {
+    renderTheme(
+      <FixedConteiner rendered={false} show={false}>
+        Children
+      </FixedConteiner>,
+    );
+
+    const conteiner = screen.getByText('Children');
+
+    expect(conteiner).toBeInTheDocument();
+    expect(conteiner).toHaveStyleRule(
+      'animation',
+      'closeShow 0.5s ease-in-out',
+    );
   });
 });
