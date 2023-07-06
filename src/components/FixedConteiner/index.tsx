9c20ad8634650c99375ea7442280ce700effb260
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-06 11:37:37 -0300

    feat(FixedConteiner):  created component

--- src/components/FixedConteiner/index.tsx
+++ src/components/FixedConteiner/index.tsx
@@ -2,8 +2,22 @@ import * as Styled from './styles';
 
 export type FixedConteinerProps = {
   children: React.ReactNode;
+  show: boolean;
+  rendered: boolean;
 };
 
-export const FixedConteiner = ({ children }: FixedConteinerProps) => {
-  return <Styled.FixedConteiner>{children}</Styled.FixedConteiner>;
+export const FixedConteiner = ({
+  children,
+  rendered,
+  show,
+}: FixedConteinerProps) => {
+  return (
+    <Styled.FixedConteiner
+      showtoggle={String(show)}
+      renderedtoggle={String(rendered)}
+      aria-label="conteiner fixed"
+    >
+      {children}
+    </Styled.FixedConteiner>
+  );
 };
--- src/components/FixedConteiner/styles.ts
+++ src/components/FixedConteiner/styles.ts
@@ -1,10 +1,56 @@
 'use client';
 
 import styled, { css } from 'styled-components';
+import { FixedConteinerProps } from '.';
 
-export const FixedConteiner = styled.section`
-  ${() => css`
+const handleShow = (show: boolean | string, rendered: boolean | string) => css`
+  ${show == 'true' &&
+  css`
+    transform: translateX(0);
+    opacity: 1;
+    visibility: visible;
+    animation: openShow 300ms ease-in-out;
+
+    @keyframes openShow {
+      0% {
+        transform: translateX(-100%);
+        opacity: 0;
+      }
+
+      100% {
+        transform: translateX(0);
+      }
+    }
+  `}
+
+  ${show == 'false' &&
+  css`
+    opacity: 0;
+    animation: closeShow 0.5s ease-in-out;
+    visibility: hidden;
+
+    @keyframes closeShow {
+      0% {
+        transform: translateX(0);
+        opacity: ${rendered == 'true' ? '1' : '0'};
+        visibility: visible;
+      }
+
+      100% {
+        transform: translate(-100%);
+        visibility: hidden;
+      }
+    }
+  `}
+`;
+
+export const FixedConteiner = styled.section<{
+  showtoggle: boolean | string;
+  renderedtoggle: boolean | string;
+}>`
+  ${({ renderedtoggle, showtoggle }) => css`
     position: fixed;
+    z-index: 4;
     left: 0;
     top: 0;
     width: 100%;
@@ -12,6 +58,9 @@ export const FixedConteiner = styled.section`
     display: flex;
     justify-content: center;
     align-items: center;
+    transform: translateX(-200%);
     background-color: rgba(0, 0, 0, 0.3);
+
+    ${handleShow(showtoggle, renderedtoggle)}
   `}
 `;
