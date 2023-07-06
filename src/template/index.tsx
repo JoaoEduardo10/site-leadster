877467ad2f095b774bded73a60dd5e0a1cb81512
João Eduardo <joaoeduardoj920@gmail.com>
2023-07-06 14:07:42 -0300

    feat(Template): adding MenuMobileConteiner

--- src/template/index.tsx
+++ src/template/index.tsx
@@ -2,6 +2,7 @@ import { NavLink } from '../components/NavLinks';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/header';
 import { MainConteiner } from '../components/MainConteiner';
+import { MenuMobileConteiner } from '../components/MenuMobileConteiner';
 
 type TemplateLeadsterProps = {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ export const TemplateLeadster = ({ children }: TemplateLeadsterProps) => {
   return (
     <>
       <Header />
+      <MenuMobileConteiner />
       <NavLink />
       <MainConteiner>{children}</MainConteiner>
       <Footer />
